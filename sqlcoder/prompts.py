bab17b87f93d273d5b7d4a997d0b0da7c51d383d
Jhoeel Luna <lunajhoeel@gmail.com>
2024-02-17 20:53:57 -0500

    refactor: fix prompt instructions and relevant table schema

--- sqlcoder/prompts.py
+++ sqlcoder/prompts.py
@@ -1,11 +1,10 @@
 sql_prompt = [
-"""### Task
-Generate a SQL query to answer [QUESTION]{question}[/QUESTION]
+"""### Instructions
+- Consider some metrics will need aggregation functions, 
+such as calculating force using mass and acceleration.
 
-### Instructions
-- If you cannot answer the question with the available database schema, return 'I do not know'
-- Remember that revenue is price multiplied by quantity
-- Remember that cost is supply_price multiplied by quantity
+### Task
+Generate a SQL query to answer [QUESTION]{question}[/QUESTION]
 
 ### Database Schema
 This query will run on a database whose schema is represented in this string:
@@ -14,6 +13,5 @@ This query will run on a database whose schema is represented in this string:
 ### Answer
 Given the database schema, here is the SQL query that answers [QUESTION]{question}[/QUESTION]
 [SQL]
-
 """
 ]
